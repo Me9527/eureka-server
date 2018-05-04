e4fb1a79c9f1d1a828dbba0adf13a0125577106b
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-05-04 16:28:21 +0800

    d

---
+++ eureka-server/src/main/java/com/example/register/Example.java
@@ -0,0 +1,16 @@
+package com.example.register;
+
+//@RestController
+//@EnableAutoConfiguration
+public class Example {
+
+//	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(Example.class, args);
+	}
+
+}
